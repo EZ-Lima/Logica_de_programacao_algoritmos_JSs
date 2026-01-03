ec1cbb863343305b250863ffb42a8870ba2c94b8
EZ-Lima <eliezerlimatj@gmail.com>
2026-01-02 21:59:52 -0300

    Start commit

---
+++ Cap2/js/ex2_1.js
@@ -0,0 +1,10 @@
+//cria a referencia ao form e ao elemento h3
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+//cria um ouvinte de evento, adicionado quando o botão submit for clicado
+frm.addEventListener("submit", (e)=>{
+    const nome = frm.inNome.value    //obtem o nome digitado no form
+    resp.innerText = `Olá ${nome}`   //exibe a resposta do programa
+    e.preventDefault()               //evita envio do form
+})
